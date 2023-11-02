62a19cf71d3c9ba4f713acf8b357adff0cf3a415
ReiTony <reimerstony@gmail.com>
2023-11-02 17:21:34 +0800

    Add student logout functionality using axios

--- Client/src/component/Logout.jsx
+++ Client/src/component/Logout.jsx
@@ -1,30 +1,53 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import axios from "axios"; 
 
 function Logout() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // Clear tokens and cookies
-    Cookies.remove("studentToken");
-    Cookies.remove("teacherToken");
+    const logoutStudent = async () => {
+      try {
+        const response = await axios.delete("http://localhost:5000/api/v1/Student/logout");
+        
+        // Check if the logout was successful
+        if (response.status === 200) {
+          Cookies.remove("studentToken");
+          Cookies.remove("accessToken");
+          Cookies.remove("refreshToken");
 
-    // Navigate to the root route
-    navigate("/");
+          navigate("/");
 
-    // Show a toast message to confirm logout
-    toast.success("You have been logged out.", {
-      position: "top-center",
-      autoClose: 3000,
-      hideProgressBar: true,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
+          toast.success("You have been logged out.", {
+            position: "top-center",
+            autoClose: 3000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+        }
+      } catch (error) {
+        console.error("Error during student logout:", error);
+
+        toast.error("An error occurred during logout.", {
+          position: "top-center",
+          autoClose: 3000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+
+      }
+    };
+
+    logoutStudent(); 
   }, [navigate]);
 
   return null;
