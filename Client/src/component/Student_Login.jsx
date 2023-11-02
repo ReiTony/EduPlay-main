99dab0c2d8ebc2e0f48204a87c92483b97c06988
ReiTony <reimerstony@gmail.com>
2023-11-02 17:23:22 +0800

    Add withCredentials option to axios post request in Student_Login component

--- Client/src/component/Student_Login.jsx
+++ Client/src/component/Student_Login.jsx
@@ -20,7 +20,10 @@ function Student_Login() {
   // Add a function to set a cookie with a JWT token
   const setTokenCookie = (token) => {
     // Set an HTTPOnly cookie with the token
-    Cookies.set("studentToken", JSON.stringify(token), { secure: true, sameSite: "strict" });
+    Cookies.set("studentToken", JSON.stringify(token), {
+      secure: true,
+      sameSite: "strict",
+    });
   };
 
   // Add a function to check if the token cookie exists
@@ -33,11 +36,15 @@ function Student_Login() {
       const userAgent = navigator.userAgent;
       const apiUrl = "http://localhost:5000/api/v1/Student/login";
 
-      const response = await axios.post(apiUrl, {
-        username: values.username,
-        password: values.password,
-        userAgent: userAgent,
-      });
+      const response = await axios.post(
+        apiUrl,
+        {
+          username: values.username,
+          password: values.password,
+          userAgent: userAgent,
+        },
+        { withCredentials: true }
+      );
 
       console.log("Response Data:", response.data.user.user);
 
@@ -72,7 +79,15 @@ function Student_Login() {
     }
   }, []); // Empty dependency array ensures this effect runs once on component mount
 
-  const { values, errors, handleBlur, handleChange, handleSubmit, touched, isSubmitting } = useFormik({
+  const {
+    values,
+    errors,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+    touched,
+    isSubmitting,
+  } = useFormik({
     initialValues: {
       username: "",
       password: "",
@@ -87,33 +102,55 @@ function Student_Login() {
       <main className="w-full md:w-[80%] lg:w-[70%] xl:w-[60%] text-center grid grid-cols-[35%_65%] mt-24 mb-16">
         <div className="grid grid-rows-[40%_15%_35%] text-white bg-[#252525] bg-opacity-95">
           <div className="flex items-center justify-center">
-            <img className="object-cover w-fit h-[90%] m-0" src={logo} alt="Logo" />
+            <img
+              className="object-cover w-fit h-[90%] m-0"
+              src={logo}
+              alt="Logo"
+            />
           </div>
           <div>
             <h1 className="text-6xl font-bold font-reemkufifont">EDUPLAY</h1>
           </div>
           <div className="flex items-center justify-center">
-            <img className="object-cover w-fit h-[90%]" src={boygirl} alt="Logo" />
+            <img
+              className="object-cover w-fit h-[90%]"
+              src={boygirl}
+              alt="Logo"
+            />
           </div>
         </div>
 
         <section className="bg-[#f7d538] opacity-95 flex flex-col gap-14 justify-center px-16 py-4">
-          <h2 className="font-extrabold px-14 text-6xl font-expletus my-8">Student Login</h2>
+          <h2 className="font-extrabold px-14 text-6xl font-expletus my-8">
+            Student Login
+          </h2>
           <form onSubmit={handleSubmit}>
-            <label htmlFor="username" className={`block mb-2 text-xl font-semibold ${touched.username && errors.username ? "text-red-500" : ""}`}>
+            <label
+              htmlFor="username"
+              className={`block mb-2 text-xl font-semibold ${
+                touched.username && errors.username ? "text-red-500" : ""
+              }`}
+            >
               Username
             </label>
             <input
               id="username"
               name="username"
               type="text"
-              className={`w-[100%] rounded-full flex p-4 px-10 mt-2 text-xl bg-black text-white border-2 font-kumbh ${touched.username && errors.username ? "border-red-500 " : ""}`}
+              className={`w-[100%] rounded-full flex p-4 px-10 mt-2 text-xl bg-black text-white border-2 font-kumbh ${
+                touched.username && errors.username ? "border-red-500 " : ""
+              }`}
               value={values.username}
               onChange={handleChange}
               onBlur={handleBlur}
               disabled={isSubmitting}
             />
-            <label htmlFor="password" className={`block mt-4 mb-2 text-xl font-semibold ${touched.password && errors.password ? "text-red-500" : ""}`}>
+            <label
+              htmlFor="password"
+              className={`block mt-4 mb-2 text-xl font-semibold ${
+                touched.password && errors.password ? "text-red-500" : ""
+              }`}
+            >
               Password
             </label>
             <div className="relative">
@@ -121,14 +158,23 @@ function Student_Login() {
                 id="password"
                 name="password"
                 type={showPassword ? "text" : "password"}
-                className={`w-[100%] justify-center flex items-center rounded-full px-10 border-2 p-4 text-xl bg-black text-white font-kumbh ${touched.password && errors.password ? "border-red-500 " : ""}`}
+                className={`w-[100%] justify-center flex items-center rounded-full px-10 border-2 p-4 text-xl bg-black text-white font-kumbh ${
+                  touched.password && errors.password ? "border-red-500 " : ""
+                }`}
                 value={values.password}
                 onChange={handleChange}
                 onBlur={handleBlur}
                 disabled={isSubmitting}
               />
-              <span className="absolute text-xl cursor-pointer top-7 right-5 " onClick={() => setShowPassword(!showPassword)}>
-                {showPassword ? <FaEye className="text-white" /> : <FaEyeSlash className="text-white" />}
+              <span
+                className="absolute text-xl cursor-pointer top-7 right-5 "
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? (
+                  <FaEye className="text-white" />
+                ) : (
+                  <FaEyeSlash className="text-white" />
+                )}
               </span>
             </div>
             <div className="flex mt-4">
@@ -140,7 +186,8 @@ function Student_Login() {
             <button
               className="font-sourceSans3 text-center rounded-full px-12 py-3 mt-4 text-2xl bg-black shadow-lg hover:shadow-green-400 text-white placeholder-white font-bold transition duration-300 ease-in-out transform hover:scale-105"
               type="submit"
-              disabled={isSubmitting}>
+              disabled={isSubmitting}
+            >
               {isSubmitting ? "Signing in..." : "Sign In"}
             </button>
           </form>
