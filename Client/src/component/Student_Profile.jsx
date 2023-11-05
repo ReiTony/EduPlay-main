d25d572fd1a3e9be5ddb8e81c95c0209f6b6adbd
j-sabile <jericosabile@gmail.com>
2023-11-06 00:45:19 +0800

    support for showing module achievements

--- Client/src/component/Student_Profile.jsx
+++ Client/src/component/Student_Profile.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 
 function StudentProfile() {
   const [studentData, setStudentData] = useState(null);
-  const [achievements, setAchievements] = useState([]);
+  const [achievements, setAchievements] = useState(null);
 
   useEffect(() => {
     const userId = localStorage.getItem("userId");
@@ -18,7 +18,6 @@ function StudentProfile() {
       })
       .then((res) => {
         setStudentData(res.data);
-        console.log("Achievements State:", res.data);
         fetchAchievements(userId);
       })
       .catch((err) => alert(err.message));
@@ -28,8 +27,6 @@ function StudentProfile() {
     axios
       .get(`${import.meta.env.VITE_API}student/achievement/${userId}`)
       .then((res) => {
-        console.log("Achievements Response:", res.data);
-
         if (Array.isArray(res.data)) {
           setAchievements(res.data);
         } else {
@@ -41,66 +38,47 @@ function StudentProfile() {
 
   const getBadge = (score, total, gradeLevel, moduleNumber) => {
     const percentage = score / total;
-    if (score === total)
-      return `/badges/Grade ${gradeLevel}/G${gradeLevel}M${moduleNumber} Gold.png`;
-    else if (percentage >= 0.7)
-      return `/badges/Grade ${gradeLevel}/G${gradeLevel}M${moduleNumber} Silver.png`;
-    else if (percentage >= 0.4)
-      return `/badges/Grade ${gradeLevel}/G${gradeLevel}M${moduleNumber} Bronze.png`;
+    if (score === total) return `/badges/Grade ${gradeLevel}/G${gradeLevel}M${moduleNumber} Gold.png`;
+    else if (percentage >= 0.7) return `/badges/Grade ${gradeLevel}/G${gradeLevel}M${moduleNumber} Silver.png`;
+    else if (percentage >= 0.4) return `/badges/Grade ${gradeLevel}/G${gradeLevel}M${moduleNumber} Bronze.png`;
     return "";
   };
 
-  if (!studentData)
-    return <div className="text-2xl font-bold m-auto">Loading...</div>;
+  if (!studentData) return <div className="text-2xl font-bold m-auto">Loading...</div>;
 
   return (
     <div className="backgroundYellow">
       <div className="profile-container p-4 m-4 bg-[#fff5be] rounded-3xl">
         <div className="flex items-center profile-header">
           <div className="w-40 h-40 mr-4 bg-gray-500 profile-picture aspect-square rounded-SM">
-            <img
-              src={studentDP}
-              alt="Profile"
-              className="object-cover w-full h-full rounded-lg"
-            />
+            <img src={studentDP} alt="Profile" className="object-cover w-full h-full rounded-lg" />
           </div>
           <div className="overflow-hidden font-bold profile-info">
+            <p className="text-3xl font-expletus">Name: {studentData ? `${studentData.firstName} ${studentData.lastName}` : "Loading..."}</p>
+            <p className="text-3xl font-expletus">Grade Level: {studentData ? studentData.gradeLevel : "Loading..."}</p>
             <p className="text-3xl font-expletus">
-              Name:{" "}
-              {studentData
-                ? `${studentData.firstName} ${studentData.lastName}`
-                : "Loading..."}
-            </p>
-            <p className="text-3xl font-expletus">
-              Grade Level: {studentData ? studentData.gradeLevel : "Loading..."}
-            </p>
-            <p className="text-3xl font-expletus">
-              Birthday:{" "}
-              <span>{`${months[studentData.birthMonth - 1]} ${
-                studentData.birthDay
-              }`}</span>
+              Birthday: <span>{`${months[studentData.birthMonth - 1]} ${studentData.birthDay}`}</span>
             </p>
           </div>
         </div>
 
         <div className="flex flex-row justify-between gap-8 my-8">
           {/* BADGES */}
-          <div className="flex flex-col w-full font-sourceSans3">
+          <div className="flex flex-col gap-4 w-full font-sourceSans3">
             <h1 className="text-center text-3xl font-bold">BADGES</h1>
             <div className="flex flex-wrap justify-center">
-              {studentData.assessmentRecords.map((assessment, i) => (
-                <img
-                  src={getBadge(
-                    assessment.score,
-                    assessment.total,
-                    assessment.gradeLevel,
-                    assessment.moduleNumber
-                  )}
-                  style={{ maxWidth: "150px", width: "100%" }}
-                  key={i}
-                  alt={`Badge for Module ${assessment.moduleNumber}`}
-                />
-              ))}
+              {studentData.assessmentRecords.length === 0 ? (
+                <h5 className="text-xl font-normal">No badges to display.</h5>
+              ) : (
+                studentData.assessmentRecords.map((assessment, i) => (
+                  <img
+                    src={getBadge(assessment.score, assessment.total, assessment.gradeLevel, assessment.moduleNumber)}
+                    style={{ maxWidth: "150px", width: "100%" }}
+                    key={i}
+                    alt={`Badge for Module ${assessment.moduleNumber}`}
+                  />
+                ))
+              )}
             </div>
           </div>
 
@@ -108,17 +86,14 @@ function StudentProfile() {
           <div className="flex flex-col gap-4 w-full font-sourceSans3">
             <h1 className="text-center text-3xl font-bold">ACHIEVEMENTS</h1>
             <div className="flex flex-wrap gap-4 font-semibold justify-center">
-              {achievements.length === 0 ? (
-                <p>No achievements to display.</p>
+              {achievements === null ? (
+                <h5 className="text-xl">Loading...</h5>
+              ) : achievements.length === 0 ? (
+                <h5 className="text-xl font-normal">No achievements to display.</h5>
               ) : (
                 achievements.map((achievement, i) => (
-                  <div className="p-8 bg-white rounded-2xl text-lg shadow-md" key={i}>
-                    {achievement.completed && (
-                      <>
-                        <p>{achievement.moduleOrAssessmentTitle}</p>
-                        <p>{achievement.completed}</p>
-                      </>
-                    )}
+                  <div className="p-6 bg-white rounded-2xl text-lg shadow-md" key={i}>
+                    {achievement.moduleOrAssessmentTitle}
                   </div>
                 ))
               )}
@@ -130,19 +105,6 @@ function StudentProfile() {
   );
 }
 
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 export default StudentProfile;
