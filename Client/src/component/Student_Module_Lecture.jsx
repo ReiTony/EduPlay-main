a5511c9b5dad5b84c68833f9457e779084c18fbf
j-sabile <jericosabile@gmail.com>
2023-11-06 23:10:21 +0800

    show next btn if video is finished in lecture

--- Client/src/component/Student_Module_Lecture.jsx
+++ Client/src/component/Student_Module_Lecture.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
 
 function Student_Module_Lecture() {
   const navigate = useNavigate();
   const gradeLevel = localStorage.getItem("gradeLevel");
   const { moduleNumber } = useParams();
   const [data, setData] = useState(null);
+  const [isFinished, setIsFinished] = useState(false);
 
   useEffect(() => {
     const init = async () => {
@@ -20,16 +22,22 @@ function Student_Module_Lecture() {
   }, []);
 
   return (
-    <div className="bg-[#fff5be] flex flex-col items-center m-4 mb-6 p-8 rounded-2xl h-full">
+    <div className="bg-[#fff5be] flex flex-col flex-grow items-center m-4 mb-6 p-8 rounded-2xl h-full">
       <h1 className="text-3xl font-semibold font-sourceSans3">{data?.title || ""}</h1>
 
       <hr className="bg-black h-1 w-full my-2" />
 
-      <iframe className="my-6" src={data?.videoLink || ""} width="864" height="486" allow="autoplay" />
+      <div className="flex flex-col flex-grow justify-center items-center gap-8">
+      <ReactPlayer url={data?.videoLink || ""} onEnded={() => setIsFinished(true)} controls/>
+      {isFinished && (
+        <button className="px-10 py-2 text-2xl font-bold text-center text-white bg-[#282424] rounded-full shadow-md" onClick={() => navigate(`/student/module/${moduleNumber}/review`)}>
+          NEXT
+        </button>
+      )}
 
-      <button className="px-10 py-2 text-2xl font-bold text-center text-white bg-black rounded-full" onClick={() => navigate(`/student/module/${moduleNumber}/review`)}>
-        NEXT
-      </button>
+      </div>
+
+
     </div>
   );
 }
