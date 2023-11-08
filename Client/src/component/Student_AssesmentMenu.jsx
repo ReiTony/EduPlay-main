967053507e31c47c87b857135ecfa25df4a9c690
j-sabile <jericosabile@gmail.com>
2023-11-08 23:13:01 +0800

    redesign assessment menu

--- Client/src/component/Student_AssesmentMenu.jsx
+++ Client/src/component/Student_AssesmentMenu.jsx
@@ -15,13 +15,13 @@ function Student_AssessmentMenu() {
   }, []);
 
   return (
-    <div className="bg-[#fff5be] flex flex-col items-center m-4 mb-6 p-8 rounded-2xl h-full">
+    <div className="bg-[#fff5be] flex flex-col items-center m-4 mb-6 p-8 rounded-2xl flex-grow">
       <h3 className="text-5xl font-semibold my-2 me-auto font-sourceSans3">Assessments</h3>
       <hr className="bg-black h-1 w-full" />
 
-      <div className="flex flex-col gap-4 h-full justify-center my-6">
+      <div className="flex flex-col gap-4 h-full justify-center my-6 w-full">
         {modules?.map((module, index) => (
-          <button className="text-2xl px-6 py-2 font-bold text-white bg-blue-500 rounded-lg hover:bg-blue-700" onClick={() => navigate(`/student/module/${module.number}/assessment`)} key={index}>
+          <button className="text-2xl px-8 py-3 font-bold bg-[#ffbc5c] text-start rounded-full w-full" onClick={() => navigate(`/student/module/${module.number}/assessment`)} key={index}>
             {`Module ${module.number}: ${module.title}`}
           </button>
         ))}
