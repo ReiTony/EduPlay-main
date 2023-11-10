fc9f1717c987224894e047bbec73b62f51df1ad4
j-sabile <jericosabile@gmail.com>
2023-11-11 01:06:55 +0800

    show next button when ppt has ended

--- Client/src/component/Student_Module_Review.jsx
+++ Client/src/component/Student_Module_Review.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import axios from "axios";
 
 function Student_Module_Review() {
@@ -8,6 +10,7 @@ function Student_Module_Review() {
   const userId = localStorage.getItem("userId");
   const username = localStorage.getItem("username");
   const [data, setData] = useState(null);
+  const [isFinished, setIsFinished] = useState(false);
 
   useEffect(() => {
     const init = async () => {
@@ -15,9 +18,9 @@ function Student_Module_Review() {
       let res = await fetch(`/modules/grade${gradeLevel}/module${moduleNumber}/review.json`);
       setData(await res.json());
 
-      res = JSON.parse(localStorage.getItem("modules"))
-      res[moduleNumber-1].submodules[2].locked = false;
-      localStorage.setItem("modules", JSON.stringify(res))
+      res = JSON.parse(localStorage.getItem("modules"));
+      res[moduleNumber - 1].submodules[2].locked = false;
+      localStorage.setItem("modules", JSON.stringify(res));
     };
     init();
   }, []);
@@ -34,11 +37,19 @@ function Student_Module_Review() {
       <h1 className="text-3xl font-semibold font-sourceSans3">{data?.title || ""}</h1>
       <hr className="bg-black h-1 w-full my-2" />
 
-      <iframe className="my-6" src={data?.link || ""} width="864" height="486" allow="autoplay"></iframe>
+      <div style={{ maxWidth: "960px" }}>
+        <Carousel className="my-10" axis="horizontal" verticalSwipe="natural" showIndicators={false} showStatus="false" onChange={(e) => e === data?.slides.length - 1 && setIsFinished(true)}>
+          {data?.slides.map((slide, ind) => (
+            <img key={ind} src={slide} />
+          ))}
+        </Carousel>
+      </div>
 
-      <button className="px-10 py-2 text-2xl font-bold text-center text-white bg-black rounded-full" onClick={handleNext}>
-        NEXT
-      </button>
+      {isFinished && (
+        <button className="px-10 py-2 text-2xl font-bold text-center text-white bg-black rounded-full" onClick={handleNext}>
+          NEXT
+        </button>
+      )}
     </div>
   );
 }
