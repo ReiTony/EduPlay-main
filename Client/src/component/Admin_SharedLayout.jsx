fe926dc245829c896501918c837a46a472adf122
j-sabile <jericosabile@gmail.com>
2023-11-28 12:29:06 +0800

    add admin navbar and sharedlayout

---
+++ Client/src/component/Admin_SharedLayout.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import Admin_NavBar from "./Admin_NavBar";
+
+function Admin_SharedLayout() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!localStorage.getItem("userId") || localStorage.getItem("userType") !== "admin") navigate("/");
+  }, []);
+
+  return (
+    <div className="flex flex-col flex-grow min-h-screen bg-[#f0e4e4]">
+      <Admin_NavBar />
+      <Outlet />
+    </div>
+  );
+}
+
+export default Admin_SharedLayout;
