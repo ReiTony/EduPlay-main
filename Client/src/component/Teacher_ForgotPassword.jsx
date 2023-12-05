c6e70d3ec5232aacfae410fcafe67fcd409c0241
j-sabile <jericosabile@gmail.com>
2023-12-06 00:22:20 +0800

    fix forgot password in mobile

--- Client/src/component/Teacher_ForgotPassword.jsx
+++ Client/src/component/Teacher_ForgotPassword.jsx
@@ -25,22 +25,18 @@ function Teacher_ForgotPassword() {
     <>
       <div className="flex flex-col justify-between min-h-screen background">
         <NavBar />
-        <main className="w-[80%] md:w-[80%] lg:w-[70%] xl:w-[60%] text-center grid lg:grid-cols-[35%_65%] m-auto">
-          <div className="grid grid-rows-[40%_15%_35%] text-white bg-[#252525]">
-            <div className="flex items-center justify-center">
-              <img className="object-cover w-fit h-[90%] m-0" src={logo} alt="Logo" />
-            </div>
-            <div>
-              <h1 className="text-6xl font-bold font-reemkufifont">EDUPLAY</h1>
-            </div>
-            <div className="flex items-center justify-center">
-              <img className="object-cover w-fit h-[90%]" src={boygirl} alt="Logo" />
-            </div>
+        <main className="w-[90%] md:w-[80%] lg:w-[70%] xl:w-[60%] text-center grid lg:grid-cols-[35%_65%] m-auto ">
+          <div className="hidden bg-[#252525] rounded-l-lg p-4 xl:p-10 lg:flex flex-col justify-center items-center gap-2">
+            <img src={logo} alt="logo" />
+            <h1 className="text-5xl text-white font-bold font-reemkufifont">EDUPLAY</h1>
+            <img src={boygirl} alt="Logo" />
           </div>
-          <form className="bg-[#2596be] flex flex-col gap-6 justify-center items-center px-16 py-4 font-sourceSans3">
-            <h2 className="my-8 text-6xl font-extrabold px-14">Teacher Forgot Password</h2>
+
+          <form className="bg-[#2596be] flex flex-col gap-2 justify-center items-center px-4 sm:px-16 py-6 font-sourceSans3">
+            <img src="/images/ijms_logo.png" className="w-40 mx-auto" alt="logo of IJMS" />
+            <h2 className="mt-2 mb-6 text-4xl sm:text-5xl font-extrabold">Teacher Forgot Password</h2>
             <input type="text" className="bg-[#282424] w-full shadow-md rounded-full font-semibold text-2xl text-white px-8 py-3" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <button type="submit" className="bg-[#282424] rounded-full shadow-md text-white text-2xl font-bold px-10 py-3 mt-10 hover:brightness-90" onClick={handleResetPassword} disabled={isSending}>
+            <button type="submit" className="bg-[#282424] rounded-full shadow-md text-white text-2xl font-bold px-10 py-3 mt-8 hover:brightness-90" onClick={handleResetPassword} disabled={isSending}>
               {isSending ? "SENDING EMAIL..." : "SEND EMAIL"}
             </button>
           </form>
