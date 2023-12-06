8e1d1b2e75e100e6a483e3f7c00ecf5d18ea0dd1
j-sabile <jericosabile@gmail.com>
2023-12-06 22:37:48 +0800

    add IJMS logo in navbar

--- Client/src/component/NavBar.jsx
+++ Client/src/component/NavBar.jsx
@@ -5,10 +5,9 @@ function NavBar() {
   const navigate = useNavigate();
 
   return (
-    <nav className="z-10 flex content-center justify-between w-full px-4 py-2 lg:px-48 md:px-12 bg-[#f7d538]  top-0 bg-opacity-80">
-      <div className="flex items-center">
-        <img src={myLogo} alt="Logo" className="cursor-pointer h-14" onClick={() => navigate("/")} />
-      </div>
+    <nav className="flex items-center gap-6 w-full px-4 py-2 lg:px-48 md:px-12 bg-[#f7d538] z-10">
+      <img src="/public/images/ijms_logo.png" className="w-20" alt="logo of IJMS" />
+      <img src={myLogo} alt="Logo" className="cursor-pointer h-14 mb-2" onClick={() => navigate("/")} />
     </nav>
   );
 }
