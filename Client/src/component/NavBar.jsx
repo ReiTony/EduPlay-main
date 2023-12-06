8aa4f34210a60971b183e06bd2753e9be92d6e45
j-sabile <jericosabile@gmail.com>
2023-12-06 23:13:03 +0800

    fix incorrect image logo link

--- Client/src/component/NavBar.jsx
+++ Client/src/component/NavBar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
 
   return (
     <nav className="flex items-center gap-6 w-full px-4 py-2 lg:px-48 md:px-12 bg-[#f7d538] z-10">
-      <img src="/public/images/ijms_logo.png" className="w-20" alt="logo of IJMS" />
+      <img src="/images/ijms_logo.png" className="w-20" alt="logo of IJMS" />
       <img src={myLogo} alt="Logo" className="cursor-pointer h-14 mb-2" onClick={() => navigate("/")} />
     </nav>
   );
