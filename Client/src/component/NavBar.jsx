e689937472910531a1939c6d443e88fe2176e31a
j-sabile <jericosabile@gmail.com>
2024-03-07 07:35:21 +0800

    fix unscrollable sign in pages

--- Client/src/component/NavBar.jsx
+++ Client/src/component/NavBar.jsx
@@ -3,14 +3,17 @@ import myLogo from "../assets/LandingPage/logo.png";
 
 function NavBar() {
   return (
-    <nav className="flex items-center gap-6 w-full px-4 py-2 lg:px-48 md:px-12 bg-[#f7d538] z-10">
-      <Link to="/about">
-        <img src="/images/ijms_logo.png" className="w-20" alt="logo of IJMS" />
-      </Link>
-      <Link to="/">
-        <img src={myLogo} alt="Logo" className="cursor-pointer h-14 mb-2" />
-      </Link>
-    </nav>
+    <>
+      <nav className="flex flex-row items-center justify-start px-4 py-0 md:px-12 lg:px-48 md:py-2 fixed top-0 w-full z-10 bg-[#f7d538]">
+        <Link to="/about">
+          <img src="/images/ijms_logo.png" className="w-20" alt="logo of IJMS" />
+        </Link>
+        <Link to="/">
+          <img src={myLogo} alt="Logo" className="cursor-pointer h-14 mb-2" />
+        </Link>
+      </nav>
+      <div className="h-[5rem] md:h-[5rem]" />
+    </>
   );
 }
 
