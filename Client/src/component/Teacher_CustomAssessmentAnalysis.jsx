9ea071ac717dd0ef51766ddb0ed49c272089046f
j-sabile <jericosabile@gmail.com>
2024-03-10 07:16:16 +0800

    ui improvements teacher-item-analysis

--- Client/src/component/Teacher_CustomAssessmentAnalysis.jsx
+++ Client/src/component/Teacher_CustomAssessmentAnalysis.jsx
@@ -1,9 +1,8 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
-import Accordion from "./Accordion";
-import { CircularProgressbar } from "react-circular-progressbar";
-import "react-circular-progressbar/dist/styles.css";
 import { useParams } from "react-router-dom";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import axios from "axios";
+import "react-circular-progressbar/dist/styles.css";
 
 function Teacher_CustomAssessmentAnalysis() {
   const { assessmendId } = useParams();
@@ -17,32 +16,30 @@ function Teacher_CustomAssessmentAnalysis() {
 
   return (
     <>
-      <div className="flex flex-col flex-grow gap-4 p-4">
-        <div className="bg-[#08a454] rounded-full shadow-md px-10 py-3 text-4xl font-bold font-sourceSans3">ITEM ANALYSIS</div>
-        <div className="flex flex-col bg-[#a8d4a4] flex-grow gap-4 rounded-3xl p-5 font-bold">
-          <h2 className="text-3xl">CUSTOM ASSESSMENTS ANALYSIS</h2>
-          <div className="flex flex-col flex-grow gap-4 text-xl">
-            {data === null ? (
-              <div className="flex justify-center items-center flex-grow text-3xl font-sourceSans3">LOADING...</div>
-            ) : (
-              <>
-                <h2 className="text-3xl text-center mt-4">{data.title}</h2>
-                <div className="flex flex-col mx-auto gap-4 w-full" style={{ maxWidth: "800px" }}>
-                  {data.questions.map((question, j) => (
-                    <div className="flex flex-row justify-start items-center gap-6" key={j}>
-                      <div className="flex justify-center items-center" style={{ width: 150, height: 150 }}>
-                        <CircularProgressbar value={question.analysis} text={`${question.analysis}%`} />
-                      </div>
-                      <div className="flex flex-col text-2xl">
-                        <div>{`Question: ${question.question}`}</div>
-                        <div>{`${question.analysis}% of students got this question correct.`}</div>
-                      </div>
+      <h1 className="mx-2 p-4 text-xl sm:text-4xl font-bold text-white shadow-md backgroundGreen rounded-xl font-reemkufifont">ITEM ANALYSIS</h1>
+      <div className="m-2 text-white flex flex-col flex-grow gap-4 p-2 sm:p-5 font-bold backgroundGreen rounded-xl">
+        <h2 className="text-xl sm:text-3xl">CUSTOM ASSESSMENTS ANALYSIS</h2>
+        <div className="flex flex-col flex-grow gap-4 text-xl">
+          {data === null ? (
+            <div className="flex justify-center items-center flex-grow text-3xl font-sourceSans3">LOADING...</div>
+          ) : (
+            <>
+              <h2 className="text-2xl sm:text-3xl text-center mt-4">{data.title}</h2>
+              <div className="flex flex-col mx-auto gap-4 w-full" style={{ maxWidth: "800px" }}>
+                {data.questions.map((question, j) => (
+                  <div className="flex flex-row justify-start items-center gap-6" key={j}>
+                    <div className="flex justify-center items-center" style={{ width: 150, height: 150 }}>
+                      <CircularProgressbar value={question.analysis} styles={buildStyles({ textColor: "#fff", pathColor: "#fff", trailColor: "transparent" })} text={`${question.analysis}%`} />
                     </div>
-                  ))}
-                </div>
-              </>
-            )}
-          </div>
+                    <div className="flex flex-col text-xl sm:text-2xl">
+                      <div>{`Question: ${question.question}`}</div>
+                      <div>{`${question.analysis}% of students got this question correct.`}</div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </>
+          )}
         </div>
       </div>
     </>
